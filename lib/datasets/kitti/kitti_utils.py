f613111c9dbe10224e561253400a24a16973633d
Lucasmogsan <lucassandby@gmail.com>
2024-04-23 20:09:39 +0200

    need kitti_util import

--- lib/datasets/kitti/kitti_utils.py
+++ lib/datasets/kitti/kitti_utils.py
@@ -410,3 +410,28 @@ if __name__ == '__main__':
         print ('3D center/2D center/projected 3D center:', center_3d, center_2d, center_3d_projected)
         print('alpha ---> ry ', object.alpha, calib.alpha2ry(object.alpha, center_2d[0]))
         break
+
+def draw_projected_box3d(image, qs, color=(255,255,255), thickness=2):
+    ''' Draw 3d bounding box in image
+        qs: (8,3) array of vertices for the 3d box in following order:
+            1 -------- 0
+           /|         /|
+          2 -------- 3 .
+          | |        | |
+          . 5 -------- 4
+          |/         |/
+          6 -------- 7
+    '''
+    qs = qs.astype(np.int32)
+    for k in range(0,4):
+       # Ref: http://docs.enthought.com/mayavi/mayavi/auto/mlab_helper_functions.html
+       i,j=k,(k+1)%4
+       # use LINE_AA for opencv3
+       cv2.line(image, (qs[i,0],qs[i,1]), (qs[j,0],qs[j,1]), color, thickness, cv2.LINE_AA)
+
+       i,j=k+4,(k+1)%4 + 4
+       cv2.line(image, (qs[i,0],qs[i,1]), (qs[j,0],qs[j,1]), color, thickness, cv2.LINE_AA)
+
+       i,j=k,k+4
+       cv2.line(image, (qs[i,0],qs[i,1]), (qs[j,0],qs[j,1]), color, thickness, cv2.LINE_AA)
+    return image
